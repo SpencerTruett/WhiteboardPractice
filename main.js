3daca7dd85a10e901f290c183c8d64280a890f97
Spencer Truett <srtruett1993@gmail.com>
2020-04-21 15:43:08 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// fizz-buzz exercise
+// for (i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(`${i}: fizzbuzz`);
+//   } else if (i % 3 === 0) {
+//     console.log(`${i}: fizz`);
+//   } else if (i % 5 === 0) {
+//     console.log(`${i}: buzz`);
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+// var add = m => {
+//   return (Add = n => n + m);
+// };
+
+// var addTen = add(10);
+// console.log(addTen(5));
+
+// var addSix = add(6);
+// console.log(addSix(40));
+
+// Write code that outputs the Fibonacci series
+// for (let i = 0, j = 0; i <= 100; i + j) {
+//   if (j === 0) {
+//     j++;
+//   } else {
+//     console.log(i);
+//     var sumOfPrevTwo = i + j;
+//     i = j;
+//     j = sumOfPrevTwo;
+//   }
+// }
